a8894034125c686ccba896833669ebff7e2a480e
olavlan <olav.landsverk@gmail.com>
2023-03-23 15:20:57 +0100

    Add files via upload

--- common.py
+++ common.py
@@ -1,7 +1,3 @@
-#Strings to identify the language variants, in filenames etc.
-NOB = "nob"
-NNO = "nno" 
-
 #Join list of regex patterns with or operator
 def join_patterns(patterns):
 	return "(" + "|".join(patterns) + ")"
